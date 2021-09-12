08ba6647e6a8549aeceb72296b86b147431b74b2
Daniel García <danhiel.garcia98@gmail.com>
2021-09-12 15:19:08 -0600

    Clase 2
    Incluir librerías para diseño
    Incluir firebase al proyecto
    Diseño de botón para iniciar sesión con GitHub
    Incluir botón y funcionalidad para iniciar sesión con GitHub
    Mantener sesión activa

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import '../styles/globals.css' // Estilos globales aplicados a todos los componentes cargados
+import 'antd/dist/antd.css';
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
